7df013fefb552d0a6ff52cbf7b56809356ccb069
Tania DePasquale <rawrtan@gmail.com>
2014-01-27 20:30:52 -0800

    Just a few things

--- http-proxy.cc
+++ http-proxy.cc
@@ -17,39 +17,36 @@ int main(void)
   struct sockaddr_in serv_addr;
  
   char sendBuff[1025];  
+
+  fprintf(stderr, "Started proxy");
  
   listenfd = socket(AF_INET, SOCK_STREAM, 0);
-  printf("socket retrieve success\n");
+  //fprintf("socket retrieve success\n");
   
   memset(&serv_addr, '0', sizeof(serv_addr));
   memset(sendBuff, '0', sizeof(sendBuff));
       
   serv_addr.sin_family = AF_INET;    
   serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");//htonl(INADDR_ANY); 
-  serv_addr.sin_port = htons(14886);    
+  serv_addr.sin_port = htons(15010);    
 
-  printf("server addr is %d\n", serv_addr.sin_addr.s_addr);
+  //fprintf("server addr is %d\n", serv_addr.sin_addr.s_addr);
  
   bind(listenfd, (struct sockaddr*)&serv_addr,sizeof(serv_addr));
   
   if(listen(listenfd, 10) == -1){
-      printf("Failed to listen\n");
+      fprintf(stderr, "Failed to listen\n");
       return -1;
   }
      
   
-  while(1)
-    {
+  connfd = accept(listenfd, (struct sockaddr*)NULL ,NULL); // accept awaiting request
+  fprintf(stderr, "Connection accepted");
 
-      connfd = accept(listenfd, (struct sockaddr*)NULL ,NULL); // accept awaiting request
-  
-      strcpy(sendBuff, "Message from server");
-      write(connfd, sendBuff, strlen(sendBuff));
- 
-      close(connfd);    
-      sleep(1);
-    }
- 
- 
+  strcpy(sendBuff, "Message from server");
+  write(connfd, sendBuff, strlen(sendBuff));
+
+  close(connfd);
+  close(listenfd);
   return 0;
 }
